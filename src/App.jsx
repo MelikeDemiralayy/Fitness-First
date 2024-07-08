63abad03bff06518b4e23a374231e9a71414b41b
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-07-08 21:52:27 +0300

    Add JoinForm component and integrate React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/home/home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+import JoinPage from "./pages/joinPage";
 
 
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/join" element={<JoinPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/joinPage/index.jsx
@@ -0,0 +1,13 @@
+// src/pages/join pages/JoinPage.js
+
+import JoinForm from "../../components/form/joinForm";
+
+function JoinPage() {
+  return (
+    <div className="join-page">
+      <JoinForm />
+    </div>
+  );
+}
+
+export default JoinPage;
