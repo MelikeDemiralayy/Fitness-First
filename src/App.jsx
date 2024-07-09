811cec34cdcc5e3d105f8e078728dd061b358b40
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-07-09 14:36:06 +0300

    Fix routing issue by updating router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import JoinPage from "./pages/joinPage";
 import VisitUsPage from "./pages/visit";
--- src/data/navbarData.js
+++ src/data/navbarData.js
@@ -1,11 +1,11 @@
 const navbarData = {
   brand: "Navbar scroll",
   links: [
-    { href: "/", text: "Home" },
+    { href: "#", text: "Home" },
     { href: "#about", text: "About" },
-    { href: "/join", text: "Join Now" },
-    { href: "/visit", text: "Visit Us" },
-    { href: "/blog", text: "Blog" },
+    { href: "#join", text: "Join Now" },
+    { href: "#visit", text: "Visit Us" },
+    { href: "#blog", text: "Blog" },
   ],
 };
   
